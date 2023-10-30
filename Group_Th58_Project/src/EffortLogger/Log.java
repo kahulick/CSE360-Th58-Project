c26eb603b29436982cf5dbd1ea003d7111c0f934
Kaelyn Hulick <kaelynhulick@gmail.com>
2023-10-29 20:51:26 -0700

    Getters and Setters

--- Group_Th58_Project/src/EffortLogger/Log.java
+++ Group_Th58_Project/src/EffortLogger/Log.java
@@ -22,11 +22,62 @@ public class Log {
 		this.category = category;
 		this.deliverable = deliverable;
 	}
+	public void setProject(String projectName) {
+		this.project = projectName;
+	}
+	public String getProject() {
+		return project;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDeltaTime(String deltaTime) {
+		this.deltaTime = deltaTime;
+	}
+	public String getDeltaTime() {
+		return deltaTime;
+	}
+	public void setlifeCycle(String lifeCycle) {
+		this.lifeCycle = lifeCycle;
+	}
+	public String getLifeCycle(String lifeCycle) {
+		return lifeCycle;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setDeliverable(String deliverable) {
+		this.deliverable = deliverable;
+	}
+	public String getDeliverable(String deliverable) {
+		return deliverable;
+	}
+	
+	
+	
+	
+	
 	
 	
 	
 	public String newLog(int idk) {
 		return "made it for " + idk;
 	}
+	
+	
+	
+	
 
 }
