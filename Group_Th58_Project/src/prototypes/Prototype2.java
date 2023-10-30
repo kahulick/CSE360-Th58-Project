0cba0e7b32b27bb643cfb72e44c5a9898be15197
Kevin Ruiz <kevin.ruiz.hdez@gmail.com>
2023-10-29 22:25:03 -0700

    Uploading my prototype 2 demo, might not work off the bat, will adjust as necessary.

--- Group_Th58_Project/src/prototypes/Prototype2.java
+++ Group_Th58_Project/src/prototypes/Prototype2.java
@@ -1,5 +1,7 @@
 package prototypes;
 
+import java.util.Scanner;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -16,10 +18,53 @@ import userInterfaceJavaFX.UserInterface;
 public class Prototype2 extends Application{
 
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+		
+		String name;	
+		String user_name;
+		String user_title;
+		String user_team;
+		int user_id;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		
+		
 		System.out.println("PROTO 2!!!\nProper Anonymization For Employees");
+		
+		System.out.println("This is a prototype of how employee anonymization will work via a generalization approach.\n");
+		
+		System.out.print("Enter name: ");
+		name = scan.nextLine();
+		
+		System.out.print("Enter user name: ");
+		user_name = scan.nextLine();
+		
+		System.out.print("Enter numerical user ID: ");
+		user_id = scan.nextInt();
+		
+		System.out.print("Enter job title: ");
+		user_title = scan.nextLine();
+		user_title = scan.nextLine();
+		
+		System.out.print("Enter affiliated team for log: ");
+		user_team = scan.nextLine();
+		
+		
+		System.out.println("Stripping identifying information...\n");
+		System.out.print("Removing name...\nRemoving user name...\nRemoving user ID...\n\n");
+		
+		System.out.println("Data being transferred:\nJob Title: " + user_title + "\nAffiliated Teams: " + user_team +"\n");	
+		
+			
+			
 
-		System.out.println("Test test");
+		}
+
+	}
+
+		
+		
+		
 
 //
 //
